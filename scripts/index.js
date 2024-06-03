f3b8f2784a7312378dce02e009921a6a1cedf756
Tatiana Shelest <shelestsd@yandex.ru>
2024-06-03 21:57:32 +1000

    bugs fixed

--- scripts/index.js
+++ scripts/index.js
@@ -8,15 +8,15 @@ const placesList = document.querySelector('.places__list');
 
 
 // @todo: Функция создания карточки
-function addCard(item, deleteCard) {
-    let cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+function createCard(item, deleteCard) {
+    const cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+    const deleteButton = cardItem.querySelector('.card__delete-button');
     cardItem.querySelector('.card__image').src = item.link;
     cardItem.querySelector('.card__image').alt = item.name;
     cardItem.querySelector('.card__title').textContent = item.name;
 
-    deleteButton.addEventListener('click', function (evt) {
-        deleteCard(evt)
-    });
+    deleteButton.addEventListener('click', deleteCard);
+
     return cardItem;
 };
 
@@ -29,12 +29,5 @@ function deleteCard(evt) {
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function(item) {
-    placesList.append(addCard(item, deleteCard));
+    placesList.append(createCard(item, deleteCard));
   });
-
-
-
-
-
-
-
