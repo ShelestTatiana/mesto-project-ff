4ca357c7ea15a81de6e514a366675daeeabae354
Tatiana Shelest <shelestsd@yandex.ru>
2024-06-03 13:32:03 +1000

    first commit

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
+
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
+
+
 
 // @todo: Функция создания карточки
+function addCard(item, deleteCard) {
+    let cardItem = cardTemplate.querySelector(".card").cloneNode(true);
+    cardItem.querySelector('.card__image').src = item.link;
+    cardItem.querySelector('.card__image').alt = item.name;
+    cardItem.querySelector('.card__title').textContent = item.name;
+
+    deleteButton.addEventListener('click', function (evt) {
+        deleteCard(evt)
+    });
+    return cardItem;
+};
+
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+};
+
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    placesList.append(addCard(item, deleteCard));
+  });
+
+
+
+
+
+
+
